f4b16c538cd5b9dab336724254b9a6dc4ecafd22
akpe2003 <aes_akpe@yahoo.ca>
2026-05-29 15:41:25 +0100

    Add files via upload

---
+++ code5.py
@@ -0,0 +1,6 @@
+first_word=str(input("Please enter a string here. Anyword really: "))
+print()
+print(f"The original word is: {first_word}")
+print()
+second_word=first_word[::-1]
+print(f"The word when reversed is: {second_word}")    
