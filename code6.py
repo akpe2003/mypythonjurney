350a3d8a990ce9eb04cb85761327cb120a5215c6
akpe2003 <aes_akpe@yahoo.ca>
2026-05-29 16:23:03 +0100

    Add files via upload

---
+++ code6.py
@@ -0,0 +1,19 @@
+sentence = "Learning Python is fun!"
+print(f"The string we are analysing is: {sentence}.")
+vowels="aeiou"
+vowels_count=0
+for x in sentence.lower():
+    if x in vowels:
+        vowels_count +=1
+        
+print(f"The total of vowel in the statement is: {vowels_count}")
+
+    
+
+
+
+
+
+
+
+
